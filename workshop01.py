7de915519927add9cb7fd0c7f9544ec8faf80c70
Korawich2549 <jamekunza123@gmail.com>
2025-08-15 14:47:49 +0700

    workshop v1

---
+++ workshop01.py
@@ -0,0 +1,8 @@
+# Commentคืออะไรที่dev พิมพ์บนลงไปใน Source code แต่ไม่มีผลต่อการทำงาน
+#Python - comment มี 2 ประเภท
+#1. Single Line comment เขียนอยู่หลังเครื่องหมาย #
+#wow wow wow
+print("hello.....")
+
+
+#2.Multi Line comment เขียนอยู่ระหว่าง"""aaaaaaa"""และ'''aaaaaa''' 
