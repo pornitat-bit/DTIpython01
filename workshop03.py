e3ad508e4ccfcdb46d6db47a5f8c163c3e9c2e80
Korawich2549 <jamekunza123@gmail.com>
2025-08-15 16:08:13 +0700

    V.2:code 01-04 but 05 not ok

---
+++ workshop03.py
@@ -0,0 +1,16 @@
+# คำสั่งแสดงผลข้อมูลใดๆที่หน้าจอ ใช้ฟังก์ชั่น prnit()
+print('Hello')
+print("HI")
+
+# number ตัวเลฃ
+print(1230)  #integer/int
+print(10.562) #float
+
+#boolean ตรรกะ มีแค่ 2 ตัว คือ True และ False
+print(True)
+print(False)
+
+#expression นิพจน์ คือ อะไรก็ตามที่มี process การทำงาน เช่นการคำนวณ การเปรียบเทียบ ตัวแปร 
+print(10+10)
+print(1<10)
+print('Bangna' <= 'Bangpoo')
