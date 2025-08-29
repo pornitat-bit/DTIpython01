b86be1b7ae038ea922c8d046c96a67089ce3e07b
pornitat-bit <pornitat@gmail.com>
2025-08-29 14:53:57 +0700

    V.2 10 11 12

--- workshop06.py
+++ workshop06.py
@@ -1,1 +1,33 @@
-# Operator ตัวดำเนินการ
+# Operator ตัวดำเนินการ คือ เครื่องหมาย
+# 1. Arithemetic Opt. เครื่องหมายคำนวณ
+# + - * /  **(exponential) %(mod) //(floor)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3) #หารปกติ
+print(10 % 3) #หารเอาเศษ
+print(10 // 3) #หารเอาส่วน
+print(10 ** 3)
+print('++++++++++++++++++++++++++++++++++++++++++')
+# 2. comparison Opt. เครื่องหมายเปรียบเทียบ ผลลัพธ์มีแค่ True หรือ False
+# == , != , > , < , >= , <= 
+print(10 >= 25)   #False
+print(999 < 555)  #False
+print('Rayoug >= Ranong')
+print('++++++++++++++++++++++++++++++++++++++++')
+# 3. logical Opt. เครื่องหมายตรรกะ
+# not , or ,and
+# ข้อมูลที่จะใช้กับเครื่องหมายนี้มีแค่ True/False และผลลัพธ์ True/False
+print(not True)
+print(not False)
+print('++++++++++++++++++++++++++++++++++++++++')
+print(True and True) #True
+print(True and False)#False
+print(False and True)#False
+print(False and False)#False
+print('+++++++++++++++++++++++++++++++++++++++++')
+print(True or True) #True
+print(True or False)#True
+print(False or True)#True
+print(False or False)#False
+
--- workshop08.py
+++ workshop08.py
@@ -1,1 +1,16 @@
-# Assigment operator
+print('===============================')
+print('Progra SUM-Average  5  Number')
+print('===============================')
+num1 = int(input('Enter number 1 : '))
+num2 = int(input('Enter number 2 : '))
+num3 = int(input('Enter number 3 : '))
+num4 = int(input('Enter number 4 : '))
+num5 = int(input('Enter number 5 : '))
+print('===============================')
+
+sum = num1 + num2 + num3 + num4 + num5
+avg = sum / 5
+
+print(f'Sum of number is : {sum}')
+print(f'Average of 5 number is :{avg}')
+print('===============================')
